c784a0c9990def7783aad02bec1f5f0a7435d247
saranoor-bboxx <s.noor@bboxx.co.uk>
2022-11-26 20:59:33 +0500

    bye bye console

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -4,22 +4,32 @@ from pyspark.sql.types import *
 from pyspark.sql import functions as F
 from textblob import TextBlob
 import time
-
+import re
 def preprocessing(lines):
-    #words = lines.select(explode(split(lines.value,'\n')).alias("word"))
-    df=lines.withColumn("value", decode("value", 'UTF-8'))
-    df=df.withColumn("value", regexp_replace(col("value"), r'\n\r', " "))
-    df=df.withColumn("value", regexp_replace(col("value"), r"[\n\r]", " "))
-    df=df.withColumn("value", regexp_replace(col("value"), r"[\n\r]", " "))
-    df=df.withColumn("value", regexp_replace(col("value"), r"[\n\n]", " "))
+    # words = lines.select(explode(split(lines.value,'\n')).alias("word"))
+    df = lines.withColumn("value", decode("value", 'UTF-8'))
+    df = df.withColumn("value", regexp_replace(col("value"), r'\n\r', " "))
+    df = df.withColumn("value", regexp_replace(col("value"), r"[\n\r]", " "))
+    df = df.withColumn("value", regexp_replace(col("value"), r"[\n\r]", " "))
+    df = df.withColumn("value", regexp_replace(col("value"), r"[\n\n]", " "))
     df = df.withColumn('value', F.regexp_replace('value', r'http\S+', ''))
-    df=df.withColumn('value', F.regexp_replace('value', '@\w+', ''))
-    df=df.withColumn('value', F.regexp_replace('value', '#', ''))
-    df=df.withColumn('value', F.regexp_replace('value', 'RT', ''))
-    df=df.withColumn('value', F.regexp_replace('value', ':', ''))
+    df = df.withColumn('value', F.regexp_replace('value', '@\w+', ''))
+    df = df.withColumn('value', F.regexp_replace('value', '#', ''))
+    df = df.withColumn('value', F.regexp_replace('value', 'RT', ''))
+    df = df.withColumn('value', F.regexp_replace('value', ':', ''))
     df = df.withColumn('value', F.regexp_replace('value', '\"', ''))
-    return df
+    pattern = re.compile(pattern = "["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           "]+", flags = re.UNICODE)
+    df = df.withColumn('value', F.regexp_replace('value', pattern, ' '))
+    # df_mod=df.withColumn('id',split(df.value, ',', 2).getItem(0))\
+    #         .withColumn('text', split(df.value, ',', 2).getItem(1))
+    # df_mod=df_mod.withColumn("text", encode("text", 'utf-8'))
 
+    return df
 # text classification
 def polarity_detection(text):
     return TextBlob(text).sentiment.polarity
@@ -45,7 +55,7 @@ if __name__ == "__main__":
         .builder \
         .appName("StructuredNetworkWordCount") \
         .getOrCreate()
-    # Create DataFrame representing the stream of input lines from connection to localhost:9999
+
     lines = spark \
         .readStream \
         .format("socket") \
@@ -53,22 +63,15 @@ if __name__ == "__main__":
         .option("port", 9999) \
         .load()
     words=preprocessing(lines)
-    # query =words\
-    #     .writeStream \
-    #     .outputMode("update") \
-    #     .trigger(processingTime='0 seconds')\
-    #     .format("console") \
-    #     .start()
-
+    words.printSchema()
     query =words\
         .writeStream \
         .outputMode("append") \
-        .trigger(processingTime='50 seconds') \
+        .trigger(processingTime='200 seconds') \
         .format("csv") \
-        .option("path", "output/filesink") \
-        .option("checkpointLocation", "/tmp/destination/checkpoint")\
+        .option("path", "output/filesink_2") \
+        .option("checkpointLocation", "/tmp/destination/checkpoint_2")\
         .start()
 
-    #time.sleep(100)
-    query.awaitTermination(60)
+    query.awaitTermination(260)
     query.stop()
